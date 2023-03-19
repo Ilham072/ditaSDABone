3a3624ff4e76b904c4e39dd38e75b94527642b05
Muhammad Ilham Nur Furqan <ilhamfurqan58@gmail.com>
2023-03-19 12:39:15 +0700

    proyek_rpl

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return(
+        <nav>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#">Beranda</a></li>
+                <li><a href="#">Pertanian</a></li>
+                <li><a href="#">Peternakan</a></li>
+                <li><a href="#">Perikanan</a></li>
+                <li><a href="#">Perindustrian</a></li>
+                <li><a href="#">Pariwisata</a></li>
+            </ul>
+        </nav>
+    );
+}  
+
+export default Navbar;
